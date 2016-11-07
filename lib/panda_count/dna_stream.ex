95e58155b8417789e045a21349c697b78a31f757
Paul Wilson <paulanthonywilson@gmail.com>
2016-11-07 22:01:00 +0000

    streamy

--- lib/panda_count/dna_stream.ex
+++ lib/panda_count/dna_stream.ex
@@ -2,10 +2,9 @@ defmodule PandaCount.DnaStream do
 
   def countbases(stream) do
     stream
-    |> Enum.filter(& !String.starts_with?(&1, ">"))
-    |> Enum.flat_map(& String.split(&1, ""))
+    |> Stream.filter(& !String.starts_with?(&1, ">"))
+    |> Stream.flat_map(& String.graphemes(&1))
     |> Enum.reduce(%{}, fn
-      "", acc -> acc
       "\n", acc -> acc
       base, acc -> Map.update(acc, base, 1, & &1 + 1)
     end)
